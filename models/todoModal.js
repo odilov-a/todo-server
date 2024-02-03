c4a29c7af30e88ea78c9d301acb9a90461db2028
odilov-a <akbarjonodilov01@gmail.com>
2024-02-03 15:16:24 +0500

    full

--- models/todoModal.js
+++ models/todoModal.js
@@ -3,6 +3,8 @@ const todoSchema = new mongoose.Schema({
   task: { type: String },
   description: { type: String },
   completed: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
 });
+
 const Todo = mongoose.model("Todo", todoSchema);
 module.exports = Todo;
